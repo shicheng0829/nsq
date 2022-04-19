520b424f8969d7f3cc6d411cffbfdaec59ea649c
chengcshi <chengcshi@tencent.com>
2022-04-19 11:10:40 +0800

    delete valid topic and channel name regrex

--- internal/protocol/names.go
+++ internal/protocol/names.go
@@ -1,11 +1,5 @@
 package protocol
 
-import (
-	"regexp"
-)
-
-var validTopicChannelNameRegex = regexp.MustCompile(`^[\.a-zA-Z0-9_-]+(#ephemeral)?$`)
-
 // IsValidTopicName checks a topic name for correctness
 func IsValidTopicName(name string) bool {
 	return isValidName(name)
@@ -20,5 +14,5 @@ func isValidName(name string) bool {
 	if len(name) > 64 || len(name) < 1 {
 		return false
 	}
-	return validTopicChannelNameRegex.MatchString(name)
+	return true
 }
